76a6f5fa75a29bfac4c8cf75200b16c08d25a2b2
Taaakuu <fujiwarachris324@gmail.com>
2025-02-28 15:39:22 +0900

    获取合约地址

--- scripts/deployMyToken.js
+++ scripts/deployMyToken.js
@@ -1,4 +1,4 @@
-const hre = require("hardhat");
+import hre from "hardhat";
 
 async function main() {
     // 获取 MyToken 合约工厂
@@ -6,8 +6,9 @@ async function main() {
     // 部署合约（如果合约构造函数有参数，在 deploy() 中传入参数）
     const myToken = await MyToken.deploy();
     // 等待合约部署完成
-    await myToken.deployed();
+    await myToken.waitForDeployment();
 
+    const address = await myToken.getAddress();
     console.log("MyToken deployed to:", myToken.address);
 }
 
